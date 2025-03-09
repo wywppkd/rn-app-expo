bd7c8b9cece63aecd6c52b299dfd4608f8df25fb
liyb <1055025755@qq.com>
2025-03-09 20:31:42 +0800

    docs

--- app/(drawer)/_(tabs)/index/_layout.tsx
+++ app/(drawer)/_(tabs)/index/_layout.tsx
@@ -17,22 +17,9 @@ export const MaterialTopTabs = withLayoutContext<
 
 export default function TabLayout() {
   return (
-    <MaterialTopTabs initialRouteName="follow">
+    <MaterialTopTabs>
       <MaterialTopTabs.Screen name="index" options={{ title: "发现" }} />
       <MaterialTopTabs.Screen name="follow" options={{ title: "关注" }} />
     </MaterialTopTabs>
   );
 }
-
-// import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-
-// const Tab = createMaterialTopTabNavigator();
-
-// export default function MyTabs() {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name="index" />
-//       <Tab.Screen name="follow" />
-//     </Tab.Navigator>
-//   );
-// }
